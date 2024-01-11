d8b615932db37a13f240285f11eccd9c69774784
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-01-11 06:17:26 -0500

    fix: change entitity name to entity

--- src/domain/entities/user.entity.ts
+++ src/domain/entities/user.entity.ts
@@ -1,4 +1,4 @@
-export class UserEnttity {
+export class UserEntity {
 
     constructor(
         public id: string,
--- src/infrastructure/datasources/mongo-auth.datasource.ts
+++ src/infrastructure/datasources/mongo-auth.datasource.ts
@@ -1,25 +1,35 @@
-import { AuthDatasource, CustomError, RegisterUserDto, UserEnttity } from "../../domain";
+import { UserModel } from "../../data/mongodb";
+import { AuthDatasource, CustomError, RegisterUserDto, UserEntity } from "../../domain";
 
 
 
 export class MongoAuthDatasource implements AuthDatasource {
 
-    async register(registerUserDto: RegisterUserDto): Promise<UserEnttity> {
+    async register(registerUserDto: RegisterUserDto): Promise<UserEntity> {
         const { name, email, password } = registerUserDto
 
         try {
             // 1. Verificar si el correo existe
+            const emailExists = await UserModel.findOne({ email: email })
+            if (emailExists) throw CustomError.badRequest('User already exists')
 
             // 2. Hash contraseña
+            const user = await UserModel.create({
+                email: email,
+                name: name,
+                password: password,
+            })
+
+            await user.save()
 
             // 3 Mapear la respuesta a la entidad
-
-            return new UserEnttity(
-                '1',
+            // TODO: falta mapper
+            return new UserEntity(
+                user.id,
                 name,
                 email,
                 password,
-                ['ADMIN_ROLE']
+                user.roles
             )
         } catch (error) {
             if (error instanceof CustomError) {
